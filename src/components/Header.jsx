dd5cae41c890df6219dbbf15ec39a87611dc35bb
kablt--6362 <wogus6362@gmail.com>
2026-01-16 00:33:42 +0900

    header change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     <header className="header">
       <div className="header-content">
         <Link to="/" className="logo">
-          Insta
+          my-instagram
         </Link>
 
         <nav className="nav">
